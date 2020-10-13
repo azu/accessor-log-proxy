77352d1d0342a927f870359d206fae441084a2e9
azu <azuciao@gmail.com>
2020-10-13 22:29:30 +0900

    add test

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,17 @@
+import assert from "assert";
+import { createProxy } from "../src/accessor-log-proxy";
+
 describe("accessor-log-proxy", function () {
-    it("return set", () => {});
+    it("return set and proxy", () => {
+        const object = {
+            a: {
+                b: {
+                    c: 123
+                }
+            }
+        };
+        const { accessSet, proxyObject } = createProxy(object);
+        console.log(proxyObject.a.b.c);
+        assert.deepStrictEqual(accessSet, new Set(["a", "a.b", "a.b.c"]));
+    });
 });
